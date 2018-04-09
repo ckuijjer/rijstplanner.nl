216b273240d0e2ecf974be0ae6e085700f8e77b0
Casper Kuijjer <casper@kuijjer.com>
2018-04-09 21:44:59 +0200

    jsconfig

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react'
-import logo from './logo.svg'
-import './App.css'
 
 import ReactGA from 'react-ga'
 
@@ -9,17 +7,7 @@ ReactGA.pageview('/')
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    )
+    return <div />
   }
 }
 
